d3d9fe4796c3344bf05bf5e0ae1de9f7b60b1dcd
Victor Huang <103213638+vichua2006@users.noreply.github.com>
2024-08-23 10:37:29 -0300

    plotted all 3D graphs for patch 3

--- plotImage.py
+++ plotImage.py
@@ -10,17 +10,23 @@ import os
 IMGDIR = r".\images\laser_beams\patch_3\\"
 GRAPHDIR = r".\intensity_profile\patch_3\\"
 
-def main():
-    file_names = os.listdir(IMGDIR)
+def loadFiles(dir: str) -> str:
+    file_names = os.listdir(dir)
+    return file_names
 
-def graphSurface(src: str, dest: str):
-
+def loadImageAsArray(dir: str):
     # Read image. 
-    img = Image.open(src)
-
+    img = Image.open(dir)
     # Convert to numpy array
-    img_as_np = np.array(img)
-    Y_len, X_len = img_as_np.shape
+    arr = np.array(img)
+
+    return arr
+
+
+def graphSurface(src: str, dest: str, title: str):
+
+    arr = loadImageAsArray(src)
+    Y_len, X_len = arr.shape
 
     x = np.arange(X_len)
     y = np.arange(Y_len)
@@ -29,13 +35,24 @@ def graphSurface(src: str, dest: str):
     fig = plt.figure()
     ax = plt.axes(projection="3d")
 
-    surf = ax.plot_surface(X, Y, img_as_np, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    surf = ax.plot_surface(X, Y, arr, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 
     ax.set_xlabel("Horizontal Position (pixels)")
     ax.set_ylabel("Vertical Position (pixels)")
     ax.set_zlabel("Brightness")
-    plt.title("")
+    plt.title(title)
 
-    plt.show()
-    plt.savefig(dest)
+    # plt.show()
+    plt.savefig(dest, format="pdf")
+    plt.close()
 
+def main():
+    file_names = loadFiles(IMGDIR)
+    for full_name in file_names:
+        title = full_name.split(".")[0]
+        src = IMGDIR + full_name
+        dest = rf"{GRAPHDIR}\{title}.pdf"
+        graphSurface(src, dest, title)
+
+if __name__ == "__main__":
+    main()
