7796640c495e0640e125ef7dc281c9a1073e9398
Victor Huang <103213638+vichua2006@users.noreply.github.com>
2024-08-27 13:41:59 -0300

    general plotting working

--- example_scripts/LMFIT_Sine.py
+++ example_scripts/LMFIT_Sine.py
@@ -1,6 +1,7 @@
 import numpy as np
 import lmfit as lm
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def mySine(x, amp, freq, phi, C):
     # C: constant offset
@@ -10,22 +11,26 @@ def mySine(x, amp, freq, phi, C):
 ModelFile = 'Sine_Model.sav'
 
 Model1 = lm.Model(mySine)
-Pars1  = Model1.make_params(amp=1., freq=0.25, phi=1., C=0)
-Pars1['amp'].max = 3.
+Pars1  = Model1.make_params(amp=1., freq=0.25, phi=1., C=400)
+Pars1['amp'].max = 1000.
 Pars1['amp'].min = 0.
 Pars1['freq'].max = np.inf
 Pars1['freq'].min = 0.
-Pars1['phi'].max = 2*np.pi
+Pars1['phi'].max = 10*np.pi
 Pars1['phi'].min = 0.
 
-x  = np.linspace(0., 3*np.pi, 101)
-dy = np.random.normal(0, 0.1, x.size)
-y1 = mySine(x, 1, 0.25, 1, 4) + dy
+df = pd.read_csv(r"..\HalfWavePlate\raw_data\aug_27.csv").to_numpy()
+df = df[:-2]
 
-result1 = Model1.fit(y1, Pars1, x=x)
+
+x = df[ : , 1]
+y = df[ : , 2]
+
+
+result1 = Model1.fit(y, Pars1, x=x)
 print(result1.fit_report())
 
-plt.plot(x, y1, 'bo')
+plt.plot(x, y, 'bo')
 plt.plot(x, result1.best_fit, 'r-')
 
 plt.show()
